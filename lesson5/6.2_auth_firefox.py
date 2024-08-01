efc156cebc49be77d0dd8171a75322175196c90c
Marina Zlomanova <zlomanova.marina@gmail.com>
2024-08-01 16:54:14 +0800

    add 4th homework

---
+++ lesson5/6.2_auth_firefox.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+
+
+try:
+    driver.get("http://the-internet.herokuapp.com/login")
+    input_name = driver.find_element(By.ID, "username").send_keys("tomsmith")
+    sleep(2)
+    input_password = driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
+    sleep(2)
+    
+    
+    button = driver.find_element(By.TAG_NAME, "button").click()
+    sleep(3)
+        
+except Exception as ex:
+   print(ex)
+finally:
+    driver.quit()
