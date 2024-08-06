41c9e76deb373b4b96aa083f4ca979fc84625fc9
Marina Zlomanova <zlomanova.marina@gmail.com>
2024-08-06 11:26:42 +0800

    add 6.1 homework

---
+++ lesson6.1/1_test_fill_the_form.py
@@ -0,0 +1,33 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from time import sleep
+from forfillform import *
+
+def test_fill_form(chrome_browser: WebDriver):
+    chrome_browser.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
+    chrome_browser.find_element(By.NAME, "first-name").send_keys(first_name)
+    chrome_browser.find_element(By.NAME, "last-name").send_keys(last_name)
+    chrome_browser.find_element(By.NAME, "address").send_keys(address)
+    chrome_browser.find_element(By.NAME, "e-mail").send_keys(email)
+    chrome_browser.find_element(By.NAME, "phone").send_keys(phone_number)
+    chrome_browser.find_element(By.NAME, "zip-code").send_keys(zip_code)
+    chrome_browser.find_element(By.NAME, "city").send_keys(city)
+    chrome_browser.find_element(By.NAME, "country").send_keys(country)
+    chrome_browser.find_element(By.NAME, "job-position").send_keys(job_position)
+    chrome_browser.find_element(By.NAME, "company").send_keys(company)
+
+    WebDriverWait(chrome_browser, 50, 3).until(EC.element_to_be_clickable((By.TAG_NAME, "button"))).click()
+    sleep(3)
+    
+    assert "alert-danger" in chrome_browser.find_element(By.ID, "zip-code").get_attribute("class")
+    assert "success" in chrome_browser.find_element(By.ID, "first-name").get_attribute("class")
+    assert "success" in chrome_browser.find_element(By.ID, "last-name").get_attribute("class")
+    assert "success" in chrome_browser.find_element(By.ID, "address").get_attribute("class")
+    assert "success" in chrome_browser.find_element(By.ID, "e-mail").get_attribute("class")
+    assert "success" in chrome_browser.find_element(By.ID, "phone").get_attribute("class")
+    assert "success" in chrome_browser.find_element(By.ID, "city").get_attribute("class")
+    assert "success" in chrome_browser.find_element(By.ID, "country").get_attribute("class")
+    assert "success" in chrome_browser.find_element(By.ID, "job-position").get_attribute("class")
+    assert "success" in chrome_browser.find_element(By.ID, "company").get_attribute("class")
